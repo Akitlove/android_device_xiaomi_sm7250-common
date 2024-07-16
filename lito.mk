2bdff0aee1366b2740c75c931a7db88bcb2b2b8b
Alec Chan <alecchangod@gmail.com>
2024-07-16 19:38:28 +0800

    Revert "sm7250-common: Move to health AIDL services"
    This reverts commit 62a38bcb0cd227a923a51e67ea7ef596cff1a00e.

--- lito.mk
+++ lito.mk
@@ -257,7 +257,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.example
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1.vendor
