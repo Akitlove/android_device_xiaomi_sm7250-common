24fe8283bce9ec9b2a337ead078a61a51a76d36e
Akitlove <416613578@qq.com>
2025-09-27 17:54:43 +0800

    sm7250-common: enable erofs,remove android.hardware.keymaster@4.1.vendor,remove emulated_storage.mk

--- properties.mk
+++ properties.mk
@@ -242,9 +242,9 @@ drm.service.enabled=true
 # Encryption
 PRODUCT_VENDOR_PROPERTIES += \
 ro.crypto.dm_default_key.options_format.version=2 \
-ro.crypto.volume.metadata.method=dm-default-key \
 ro.crypto.volume.filenames_mode=aes-256-cts \
-ro.crypto.volume.options=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized
+ro.crypto.volume.metadata.method=dm-default-key \
+ro.crypto.volume.options=::v2
 
 # Fingerprint
 PRODUCT_VENDOR_PROPERTIES += \
