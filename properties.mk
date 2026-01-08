ac9bff6971523ab5d482def831169c031a4a312d
Akitlove <416613578@qq.com>
2026-01-08 17:42:11 +0800

    sm7250-common: Remove Force triple frame buffers * Causing some apps laggy & UI performance regression
    Change-Id: I1a88503cdfbb6c52fbb66d6f67ffe26d9c51043c
    Co-Authored-By: Reinazhard <reinazhard@gmail.com>
    Co-Authored-By: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: Amrito | アンブロシア <bhabanidas431@gmail.com>

--- properties.mk
+++ properties.mk
@@ -196,7 +196,6 @@ persist.sys.sf.color_mode=9 \
 persist.sys.sf.color_saturation=1.0 \
 persist.sys.sf.native_mode=2 \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 ro.surface_flinger.max_virtual_display_dimension=4096 \
 ro.surface_flinger.protected_contents=true \
 ro.surface_flinger.support_kernel_idle_timer=true \
