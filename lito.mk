328833896d691ca3806868de9fbc92a499410e59
Akitlove <416613578@qq.com>
2026-01-24 17:17:06 +0800

    sm7250-common: Revert Build AIDL NXP NFC HAL" Reason: Multiple crashes of the android.hardware.nfc-service.nxp service.

--- lito.mk
+++ lito.mk
@@ -365,7 +365,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.nxp \
+    android.hardware.nfc@1.2-service \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor \
