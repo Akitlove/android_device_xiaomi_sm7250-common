a2c4ff5f6267abc40b36fc9c2875bfd1b185b6dd
Akitlove <416613578@qq.com>
2026-01-26 09:20:24 +0800

    sm7250-common: Migrate mount point creation out of Android.mk

--- lito.mk
+++ lito.mk
@@ -398,6 +398,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_SUPER_PARTITION := true
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Perf
 PRODUCT_PACKAGES += \
     libqti-perfd-client
